953da24248fd0762a6728de9c98ad1bcf782365e
Ming <minghsieh@datacom.com.tw>
2016-09-22 14:43:15 +0800

    update id

--- app/src/main/java/net/sourceforge/simcpux/Constants.java
+++ app/src/main/java/net/sourceforge/simcpux/Constants.java
@@ -2,7 +2,7 @@ package net.sourceforge.simcpux;
 
 public class Constants {
 	// APP_ID 替换为你的应用从官方网站申请到的合法appId
-    public static final String APP_ID = "wxa86249ef7e0f3da8";
+    public static final String APP_ID = "wx814a1ce74e6790f7";
     
     public static class ShowMsgActivity {
 		public static final String STitle = "showmsg_title";
